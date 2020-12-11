143255be377f47ae5c5999a4870c0aed5d9168b2
AnnegretJanzso <annegret.janzso@dfki.de>
2020-12-11 18:47:06 +0100

    new files for generalization/avoiding import circulation

---
+++ german_wiktionary/Use_BERT.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+"""
+Last Updated on Sun December 10 17:29 2020
+
+@author: Annegret
+"""
+
+
+import torch
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+import pandas as pd # for building the file
+from de_preprocessing import *
+import sys
+from tqdm import tqdm
+import numpy as np
+
+def get_marked_text_from_examples(sentence):
+    # get example sentences with special tokens for beginning and end of sentences
+    return "[CLS] " + sentence + " [SEP]"
+
+def run_BERT(word,tokenizer ,text, model):
+    # print(word,text)
+    word_position = text.split().index(word) + 1
+    # Split the sentence into tokens.
+    tokenized_text = tokenizer.tokenize(text)
+    # Map the token strings to their vocabulary indeces.
+    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+    # print(indexed_tokens)
+
+    tokens_tensor = torch.tensor([indexed_tokens])
+    # print(tokens_tensor)
+    # Mark each of the tokens as belonging to sentence 1
+    segments_ids = [1] * len(tokenized_text)
+    # print(segments_ids)
+    # Convert inputs to PyTorch tensors
+    segments_tensors = torch.tensor([segments_ids])
+    # print(segments_tensors)
+    # Put the model in "evaluation" mode, meaning feed-forward operation.
+    model.eval()
+    # Predict hidden states features for each layer
+    with torch.no_grad():
+        encoded_layers, _ = model(tokens_tensor, segments_tensors)
+        # print(encoded_layers)
+    layer_word = 0
+
+    batch_word = 0
+
+    token_word = 0
+
+    # For the 5th token in our sentence, select its feature values from layer.
+    token_word = word_position
+    layer_word = word_position
+    try:
+        vec_word = encoded_layers[layer_word][batch_word][token_word]
+    except IndexError:
+        vec_word = np.nan
+
+    # `encoded_layers` is a Python list.
+    # Each layer in the list is a torch tensor.
+    # Concatenate the tensors for all layers. We use `stack` here to
+    # create a new dimension in the tensor.
+    token_embeddings = torch.stack(encoded_layers, dim=0)
+    # Remove dimension 1, the "batches".
+    token_embeddings = torch.squeeze(token_embeddings, dim=1)
+
+    # Swap dimensions 0 and 1.
+    token_embeddings = token_embeddings.permute(1,0,2)
+
+    # Stores the token vectors, with shape [22 x 3,072]
+    token_vecs_cat = []
+    # For each token in the sentence...
+    for token in token_embeddings:
+        
+        # `token` is a [12 x 768] tensor
+
+        # Concatenate the vectors (that is, append them together) from the last 
+        # four layers.
+        # Each layer vector is 768 values, so `cat_vec` is length 3,072.
+        cat_vec = torch.cat((token[-1], token[-2], token[-3], token[-4]), dim=0)
+        
+        # # Use `cat_vec` to represent `token`.
+        token_vecs_cat.append(cat_vec)
+
+    # sentence vector
+    token_vecs = encoded_layers[11][0]
+    # Calculate the average of all token vectors.
+    sentence_embedding = torch.mean(token_vecs, dim=0)
+    return (vec_word,sentence_embedding)
+
+def process_number(relevant_pairs,entry_dict,model,tokenizer,frame):
+    for (singular,plural) in tqdm(relevant_pairs):
+        # print(singular)
+        word_examples = entry_dict[singular]["examples"]
+        plural_examples = entry_dict[plural]["examples"]
+        word_senses = entry_dict[singular]["senses"]
+        plural_senses = entry_dict[plural]["senses"]
+        gender_sg = entry_dict[singular]["gender"]
+        gender_pl = entry_dict[plural]["gender"]
+        sg_flections = entry_dict[singular]["flections"]
+        pl_flections = entry_dict[plural]["flections"]
+        for w_index in word_senses.keys():
+            sense = word_senses[w_index]
+            for example in word_examples[w_index]:
+                marked_text = get_marked_text_from_examples(example)
+                if singular in marked_text.split():
+                    (word_vector,sentence_embedding) = run_BERT(singular,tokenizer,marked_text,model)
+                elif any([f in marked_text.split() for f in sg_flections]):
+                    for f in sg_flections:
+                        if f in marked_text.split():
+                            (word_vector,sentence_embedding) = run_BERT(singular,tokenizer,marked_text,model)
+                else:
+                    continue
+                frame = frame.append({"Word":singular,"Number":"Sg","Gender":gender_sg,"Sense":sense,"Sentence":example,"Word Vector": word_vector, "Sentence Vector": sentence_embedding},ignore_index=True)
+        for p_index in plural_senses.keys():
+            sense = plural_senses[p_index]
+            for example in plural_examples[p_index]:
+                marked_text = get_marked_text_from_examples(example)
+                if plural in marked_text.split():
+                    (word_vector,sentence_embedding) = run_BERT(plural,tokenizer,marked_text,model)
+                elif any([f in marked_text.split() for f in pl_flections]):
+                    for f in pl_flections:
+                        if f in marked_text.split():
+                            (word_vector,sentence_embedding) = run_BERT(plural,tokenizer,marked_text,model)
+                else:
+                    continue
+                frame = frame.append({"Word":plural,"Number":"Pl","Gender":gender_pl,"Sense":sense,"Sentence":example,"Word Vector": word_vector, "Sentence Vector": sentence_embedding},ignore_index=True)
+    return frame
+
+def process_gender(gender_list,entry_dict,model,tokenizer,frame):
+    for item in tqdm(gender_list):
+        examples = entry_dict[item]["examples"]
+        gender = entry_dict[item]["gender"]
+        flections = entry_dict[item]["flection"]
+        for g in gender:
+            senses = entry_dict[item]["senses"][g]
+            for index in senses:
+                current = senses[index]
+                sentence = examples[index]
+                marked_text = get_marked_text_from_examples(sentence)
+                if item in marked_text.split():
+                    (word_vector,sentence_embedding) = run_BERT(item,tokenizer,marked_text,model)
+                elif any([f in marked_text.split() for f in flections]):
+                    for f in flections:
+                        if f in marked_text.split():
+                            (word_vector,sentence_embedding) = run_BERT(f,tokenizer,marked_text,model)
+                            break
+                else:
+                    continue
+                frame = frame.append({"Word":item,"Gender":g,"Sense":current,"Sentence":sentence,"Word Vector":word_vector,"Sentence Vector":sentence_embedding},ignore_index=True)
+    return frame
+
+def process_other(other,entry_dict,model,tokenizer,frame):
+    for item in tqdm(other):
+        examples = entry_dict[item]["examples"]
+        senses = entry_dict[item]["senses"]
+        gender = entry_dict[item]["gender"]
+        flections = entry_dict[item]["flections"]
+        for index in senses.keys():
+            sense = senses[index]
+            for example in examples[index]:
+                marked_text = get_marked_text_from_examples(example)
+                if item in marked_text.split():
+                    (word_vector,sentence_embedding) = run_BERT(item,tokenizer,marked_text,model)
+                elif any([f in marked_text.split() for f in flections]):
+                    for f in flections:
+                        if f in marked_text.split():
+                            (word_vector,sentence_embedding) = run_BERT(item,tokenizer,marked_text,model)
+                else:
+                    continue
+                frame = frame.append({"Word":item,"Gender":gender,"Sense":sense,"Sentence":example,"Word Vector":word_vector,"Sentence Vector":sentence_embedding},ignore_index=True)
+    return frame
+
+def write_files(filename,tokenizer,model,mode):
+    entry_dict = read_files(filename)
+    number_pairs,gender_list, other = find_pairs(entry_dict)
+    frame = pd.DataFrame(columns=["Word", "Number", "Gender", "Sense", "Sentence", "Word Vector", "Sentence Vector"])
+    if mode == "-n": # for number
+        frame = process_number(number_pairs,entry_dict,model,tokenizer,frame)
+        frame.to_csv("number.csv",sep="\t")
+        frame.to_pickle("number.pkl")
+    elif mode == "-g": # for gender
+        frame = process_gender(gender_list,entry_dict,model,tokenizer,frame)
+        frame.to_csv("gender.csv",sep="\t")
+        frame.to_pickle("gender.pkl")
+    elif mode == "-o": # for other
+        frame = process_other(other,entry_dict,model,tokenizer,frame)
+        frame.to_csv("other.csv",sep="\t")
+        frame.to_pickle("other.pkl")
+    else:
+        print("Invalid mode. Please choose from <-n,-o>.")
+        sys.exit()
+
+if __name__ == "__main__":
+    path = "wiktionaries\\"
+    filename = path + "enwiktionary-new.txt"
+    # Load tokenizer
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    # Load pre-trained model (weights)
+    model = BertModel.from_pretrained('bert-base-uncased')
+    write_files(filename,tokenizer,model,sys.argv[1])
+    
---
+++ preprocessing.py
@@ -0,0 +1,124 @@
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+import string
+import sys
+from tqdm import tqdm
+
+def read_files(filename):
+    with open(filename,mode="r",encoding="utf-8") as data:
+        entry_dict = dict()
+        for line in tqdm(data):
+            if line.startswith("title: "):
+                title = line[len("title: "):]
+                title = title.strip("\n")
+                if " " in title or "-" in title: # can't be kept due to removal of punctuation and splitting of sentences later on
+                    continue
+                if title not in entry_dict.keys():
+                    entry_dict[title] = defaultdict(set)
+                    entry_dict[title]["examples"] = defaultdict(set)
+                    entry_dict[title]["senses"] = dict()
+            elif line.startswith("\tgender: ") and title in entry_dict.keys(): # German and Spanish only
+                genus = line[len("\tgender: "):]
+                genus = eval(genus)
+                for item in genus:
+                    if item not in entry_dict[title]["senses"].keys():
+                        entry_dict[title]["senses"][item] = dict()
+                    entry_dict[title]["gender"].add(item)
+            elif line.startswith("\tplural: ") and title in entry_dict.keys():
+                plural = line[len("\tplural: "):]
+                plural = eval(plural) # convert string back to list
+                for item in plural:
+                    entry_dict[title]["plural"].add(item)
+            elif line.startswith("\t\tflection: ") and title in entry_dict.keys(): # currently only German
+                flection = line[len("\t\tflection: "):]
+                flection = eval(flection) # convert string back to set
+                for item in flection:
+                    entry_dict[title]["flection"].add(item)
+            elif line.startswith("\t\tsense") and title in entry_dict.keys():
+                index = line[len("\t\tsense"):][0]
+                sense = line[len("\t\tsense" + index + ": "):]
+                try:
+                    if "gender" in entry_dict[title].keys(): # for Spansísh and German
+                        gender = entry_dict[title]["gender"]
+                        if gender == [] or gender == ["0"]: # when there is no gender given
+                            if "-" not in entry_dict[title]["senses"].keys(): # when there is a sense given
+                                entry_dict[title]["senses"]["-"] = dict()
+                            entry_dict[title]["senses"]["-"][str(index)] = sense
+                        else:
+                            for item in gender:
+                                entry_dict[title]["senses"][item][str(index)] = sense # sort senses by current gender and index
+                    else: # for English and for words without gender
+                        if "-" not in entry_dict[title]["senses"].keys():
+                            entry_dict[title]["senses"]["-"] = dict()
+                        entry_dict[title]["senses"]["-"][str(index)] = sense
+                except IndexError:
+                    continue
+            elif line.startswith("\t\t\texample(s)") and title in entry_dict.keys():
+                index = line[len("\t\t\texample(s)"):][0]
+                examples = line[len("\t\t\texample(s)" + index + ": "):]
+                examples = eval(examples)
+                if examples != []:
+                    for item in examples:
+                        x = item.split("] ",1)
+                        if len(x) >= 2:
+                            sense = x[0][-1]
+                            sentence = x[1]
+                            # punctuation and "" attached to a word can lead to not finding the word in this sentence
+                            sentence = sentence.translate(str.maketrans('', '', string.punctuation)) # remove all punctuation
+                            sentence = sentence.translate(str.maketrans("","",'“')) # remove "
+                            sentence = sentence.translate(str.maketrans("","",'„'))
+                            sentence = sentence.translate(str.maketrans("","","»"))
+                            sentence = sentence.translate(str.maketrans("","","«"))
+                            sentence = sentence.translate(str.maketrans("","","‘"))
+                            sentence = sentence.translate(str.maketrans("","","‚"))
+                            if sentence.startswith("Beispiele fehlen"): # can occur in German data
+                                continue
+                            elif title in sentence.split():
+                                entry_dict[title]["examples"][index].add(sentence)
+                            elif "flection" in entry_dict[title].keys():
+                                for item in entry_dict[title]["flection"]:
+                                    if item in sentence:
+                                        entry_dict[title]["examples"][index].add(sentence)
+                                        break
+    return entry_dict
+
+def find_sets(entry_dict):
+    gender_list = []
+    number_pairs = []
+    others = []
+    for title in tqdm(entry_dict.keys()):
+        if "plural" in entry_dict[title].keys():
+            plural = entry_dict[title]["plural"]
+            for item in plural:
+                if item == "none" or item == "unspecified": # only use existing plurals
+                    continue
+                if item in entry_dict.keys() and entry_dict[item] != entry_dict[title]: # when plural has an own entry, which is not the current entry (when plural is written the same as singular)
+                    number_pairs.append((title,item)) # add all pairs of singular and plural with own sense to list
+        if "gender" in entry_dict[title].keys(): # German and Spanish
+            gender = entry_dict[title]["gender"]
+            if len(gender) > 1:
+                gender_list.append(title)
+        try:
+            if title not in gender_list: # if title has no gender-ambiguity
+                for item in number_pairs: 
+                    if title in item:
+                        raise KeyError # when title has number-ambiguity, we don't need it here
+                others.append(title)# only continues here when title has no gender- or number-ambiguity
+        except KeyError:
+            continue
+    return (number_pairs,gender_list,others)
+
+if __name__ == "__main__":
+    language = input("Choose a language: German/Spanish/English \n ")
+    if language.lower() == "german":
+        filename = "german_wiktionary\\wiktionaries\\dewiktionary-new.txt"
+    elif language.lower() == "english":
+        filename = "english_wiktionary\\wiktionaries\\enwiktionary-new.txt"
+    elif language.lower() == "spanish":
+        filename = "spanish_wiktionary\\wiktionaries\\eswiktionary-new.txt"
+    else:
+        sys.exit()
+    print("Reading file...")
+    entry_dict = read_files(filename)
+    print("Finding relevant data...")
+    (number_pairs,gender_list,others) = find_sets(entry_dict)
