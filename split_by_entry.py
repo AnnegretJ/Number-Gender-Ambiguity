caa69ceb574fe6ae0aeb45ae151159f8499fb211
AnnegretJ <74415073+AnnegretJ@users.noreply.github.com>
2020-12-15 16:36:47 +0100

    Add files via upload
    adjustments for reproducability on windows and linux

--- split_by_entry.py
+++ split_by_entry.py
@@ -2,10 +2,11 @@ import xml.etree.ElementTree as ET
 import re
 import os
 import sys
+import time
 
 # path = "C:\\Users\\Tili\\Documents\\DFKI\\MLT\\BERT-stuff\\wiktionary\\wiktionaries\\"
 
-
+start = time.time()
 if len(sys.argv) >= 2:
     if sys.argv[1] in ["German","English","Spanish"]:
         language = sys.argv[1]
@@ -15,21 +16,39 @@ if len(sys.argv) >= 2:
         else:
             if language == "German":
                 try:
-                    path = "german_wiktionary\\wiktionaries\\"
+                    if "linux" in sys.platform:
+                        path = "german_wiktionary/wiktionaries/"
+                    elif "win" in sys.platform:
+                        path = "german_wiktionary\\wiktionaries\\"
+                    else:
+                        print("Not available for ", sys.platform)
+                        sys.exit()
                     original = path + "dewiktionary-20200501-pages-articles.xml"
                 except FileNotFoundError:
                     print("Please specify a path.")
                     sys.exit()
             elif language == "English":
                 try:
-                    path = "english_wiktionary\\wiktionaries\\"
+                    if "linux" in sys.platform:
+                        path = "english_wiktionary/wiktionaries/"
+                    elif "win" in sys.platform:
+                        path = "english_wiktionary\\wiktionaries\\"
+                    else:
+                        print("Not available for ", sys.platform)
+                        sys.exit()
                     original = path + "enwiktionary-20200501-pages-articles.xml"
                 except FileNotFoundError:
                     print("Please specify a path.")
                     sys.exit()
             elif language == "Spanish":
                 try:
-                    path = "spanish_wiktionary\\wiktionaries\\"
+                    if "linux" in sys.platform:
+                        path + "spanish_wiktionary/wiktionaries/"
+                    elif "win" in sys.platform:
+                        path = "spanish_wiktionary\\wiktionaries\\"
+                    else:
+                        print("Not available for ", sys.platform)
+                        sys.exit()
                     original = path + "eswiktionary-20200501-pages-articles.xml"
                 except FileNotFoundError:
                     print("Please specify a path.")
@@ -43,9 +62,11 @@ else:
 
 # original = path + "enwiktionary-20200501-short.xml"
 
-n = 1 # file counter
-
-
+if "win" in sys.platform:
+    win = True
+else:
+    win = False
+n = 1
 if language == "German":
     child_language_pattern = re.compile(r"\(\{\{Sprache\|(.*?)\}\}\)")
     # ({{Sprache|Latein}}) ==
@@ -56,16 +77,31 @@ if language == "German":
             e.write(b'<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.10/ http://www.mediawiki.org/xml/export-0.10.xsd" version="0.10" xml:lang="de">\n')
             try:
                 while True:
-                    print(n)
-                    if path != "german_wiktionary\\wiktionaries\\":
+                    if path != "german_wiktionary\\wiktionaries\\" and path != "german_wiktionary/wiktionaries/":
                         try:
-                            path = os.mkdir("german_wiktionary\\wiktionaries\\")
+                            if win:
+                                os.mkdir("german_wiktionary\\wiktionaries")
+                                path = "german_wiktionary\\wiktionaries\\"
+                            else:
+                                os.mkdir("german_wiktionary/wiktionaries")
+                                path = "german_wiktionary/wiktionaries/"
                         except FileExistsError:
-                            path = "german_wiktionary\\wiktionaries\\"
+                            if win:
+                                path = "german_wiktionary\\wiktionaries\\"
+                            else:
+                                path = "german_wiktionary/wiktionaries/"
                     try:
-                        title = path + "by_entry/dewiktionary_short_" + str(n)
-                    except FileNotFoundError:
-                        title = path + os.mkdir("by_entry\\") + str(n)
+                        if win:
+                            os.mkdir("german_wiktionary\\wiktionaries\\by_entry\\")
+                            title = path + "by_entry\\dewiktionary_short_" + str(n)
+                        else:
+                            os.mkdir("german_wiktionary/wiktionaries/by_entry/")
+                            title = path + "by_entry/dewiktionary_short_" + str(n)
+                    except FileExistsError:
+                        if win:
+                            title = path + "by_entry\\dewiktionary_short_" + str(n)
+                        else:
+                            title = path + "by_entry/dewiktionary_short_" + str(n)
                     filename = title + ".xml"
                     with open(filename, 'wb') as f:
                         f.write(b'<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.10/ http://www.mediawiki.org/xml/export-0.10.xsd" version="0.10" xml:lang="de">\n')
@@ -73,12 +109,8 @@ if language == "German":
                             event, elem = next(context)
                             if elem.tag == '{http://www.mediawiki.org/xml/export-0.10/}page':
                                 for child in elem.iter():
-                                    # if child.tag == '{http://www.mediawiki.org/xml/export-0.10/}title':
-                                    #     if child.text == "Kiefer":
-                                    #         print(child.text,filename)
                                     if child.tag == "{http://www.mediawiki.org/xml/export-0.10/}revision":
                                         for grandchild in child.iter():
-                                            # print(grandchild.tag)
                                             if grandchild.tag == "{http://www.mediawiki.org/xml/export-0.10/}text" and grandchild.text != None:
                                                 if child_language_pattern.search(grandchild.text):
                                                     if child_language_pattern.search(grandchild.text).group(1) == "Englisch":
@@ -99,7 +131,7 @@ if language == "German":
             except StopIteration:
                 with open(filename, "ab") as f:
                     f.write(b'</mediawiki>')
-                print(str(n) + " new files")
+            print(str(n) + " new files")
             e.write(b"</mediawiki>")
         s.write(b"</mediawiki>")
 elif language == "English":
@@ -111,16 +143,33 @@ elif language == "English":
             g.write(b'<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.10/ http://www.mediawiki.org/xml/export-0.10.xsd" version="0.10" xml:lang="de">\n')
             try:
                 while True:
-                    print(n)
-                    if path != "english_wiktionary\\wiktionaries\\":
+                    if path != "english_wiktionary\\wiktionaries\\" and path != "english_wiktionary/wiktionaries/":
                         try:
-                            path = os.mkdir("english_wiktionary\\wiktionaries\\")
+                            if win:
+                                os.mkdir("english_wiktionary\\wiktionaries\\")
+                                path = "english_wiktionary\\wiktionaries\\"
+                            else:
+                                os.mkdir("english_wiktionary/wiktionaries")
+                                path = "english_wiktionary/wiktionaries/"
                         except FileExistsError:
-                            path = "english_wiktionary\\wiktionaries\\"
+                            if win:
+                                path = "english_wiktionary\\wiktionaries\\"
+                            else:
+                                path = "english_wiktionary/wiktionaries/"
                     try:
-                        title = path + "by_entry/enwiktionary_short_" + str(n)
-                    except FileNotFoundError:
-                        title = path + os.mkdir("by_entry\\") + str(n)
+                        if win:
+                            os.mkdir("english_wiktionary\\wiktionaries\\by_entry\\")
+                            title = path + "by_entry\\enwiktionary_short_" + str(n)
+                        else:
+                            os.mkdir("english_wiktionary/wiktionaries/by_entry")
+                            title = path + "by_entry/enwiktionary_short_" + str(n)
+                    except FileExistsError:
+                        if win:
+                            new = "by_entry\\enwiktionary_short_"
+                            title = path + new + str(n)
+                        else:
+                            new = "by_entry/enwiktionary_short_"
+                            title = path + new + str(n)
                     filename = title + ".xml"
                     with open(filename, 'wb') as f:
                         f.write(b'<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.10/ http://www.mediawiki.org/xml/export-0.10.xsd" version="0.10" xml:lang="en">\n')
@@ -153,12 +202,11 @@ elif language == "English":
             except StopIteration:
                 with open(filename, "ab") as f:
                     f.write(b'</mediawiki>')
-                print(str(n) + " new files")
+            print(str(n) + " new files")
             g.write(b"</mediawiki>")
         s.write(b"</mediawiki>")
 elif language == "Spanish":
     child_language_pattern = re.compile(r"== \{\{lengua\|(.*?)\}\} ==")
-    # == {{lengua|es}} ==
     context = ET.iterparse(original, events=('end', ))
     with open("german_from_spanish_dump.xml","wb") as g:
         g.write(b'<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.10/ http://www.mediawiki.org/xml/export-0.10.xsd" version="0.10" xml:lang="es">\n')
@@ -166,16 +214,31 @@ elif language == "Spanish":
             e.write(b'<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.10/ http://www.mediawiki.org/xml/export-0.10.xsd" version="0.10" xml:lang="es">\n')
             try:
                 while True:
-                    print(n)
-                    if path != "spanish_wiktionary\\wiktionaries\\":
+                    if path != "spanish_wiktionary\\wiktionaries\\" and path != "spanish_wiktionary/wiktionaries/":
                         try:
-                            path = os.mkdir("spanish_wiktionary\\wiktionaries\\")
+                            if win:
+                                os.mkdir("spanish_wiktionary\\wiktionaries\\")
+                                path = "spanish_wiktionary\\wiktionaries\\"
+                            else:
+                                os.mkdir("spanish_wiktionary/wiktionaries/")
+                                path = "spanish_wiktionary/wiktionaries/"
                         except FileExistsError:
-                            path = "spanish_wiktionary\\wiktionaries\\"
+                            if win:
+                                path = "spanish_wiktionary\\wiktionaries\\"
+                            else:
+                                path = "spanish_wiktionary/wiktionaries/"
                     try:
-                        title = path + "by_entry/eswiktionary_short_" + str(n)
-                    except FileNotFoundError:
-                        title = path + os.mkdir("by_entry\\") + str(n)
+                        if win:
+                            os.mkdir("spanish_wiktionary\\wiktionaries\\by_entry\\")
+                            title = path + "by_entry\\eswiktionary_short_" + str(n)
+                        else:
+                            os.mkdir("spanish_wiktionary/wiktionaries/by_entry/")
+                            title = path + "by_entry/eswiktionary_short_" + str(n)
+                    except FileExistsError:
+                        if win:
+                            title = path + "by_entry\\eswiktionary_short_" + str(n)
+                        else:
+                            title = path + "by_entry/eswiktionary_short_" + str(n)
                     filename = title + ".xml"
                     with open(filename, 'wb') as f:
                         f.write(b'<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.10/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.10/ http://www.mediawiki.org/xml/export-0.10.xsd" version="0.10" xml:lang="es">\n')
@@ -183,12 +246,8 @@ elif language == "Spanish":
                             event, elem = next(context)
                             if elem.tag == '{http://www.mediawiki.org/xml/export-0.10/}page':
                                 for child in elem.iter():
-                                    # if child.tag == '{http://www.mediawiki.org/xml/export-0.10/}title':
-                                    #     if child.text == "Kiefer":
-                                    #         print(child.text,filename)
                                     if child.tag == "{http://www.mediawiki.org/xml/export-0.10/}revision":
                                         for grandchild in child.iter():
-                                            # print(grandchild.tag)
                                             if grandchild.tag == "{http://www.mediawiki.org/xml/export-0.10/}text" and grandchild.text != None:
                                                 if child_language_pattern.search(grandchild.text):
                                                     if child_language_pattern.search(grandchild.text).group(1) == "en":
@@ -209,6 +268,8 @@ elif language == "Spanish":
             except StopIteration:
                 with open(filename, "ab") as f:
                     f.write(b'</mediawiki>')
-                print(str(n) + " new files")
+            print(str(n) + " new files")
             e.write(b"</mediawiki>")
         g.write(b"</mediawiki>")
+end = time.time()
+print(str(end-start)+ " seconds")
